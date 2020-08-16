ed3b3d7155e9851d98548a0812ede7a837ad7306
Arnab <arnabd2006@gmail.com>
2020-08-17 00:16:02 +0200

    2nd Com

--- src/main/java/das/tech/studentapplication/dao/StudentData.java
+++ src/main/java/das/tech/studentapplication/dao/StudentData.java
@@ -1,9 +1,16 @@
 package das.tech.studentapplication.dao;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentData {
     @Id
     private String studentId;
--- src/main/java/das/tech/studentapplication/repository/StudentRepository.java
+++ src/main/java/das/tech/studentapplication/repository/StudentRepository.java
@@ -1,12 +1,11 @@
 package das.tech.studentapplication.repository;
 
 import das.tech.studentapplication.dao.StudentData;
-import org.reactivestreams.Publisher;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
-public interface StudentRepository extends ReactiveMongoRepository<StudentData, String> {
-    Flux<StudentData> findAllById(String studentId);
+public interface StudentRepository extends ReactiveCrudRepository<StudentData, String> {
+    Mono<StudentData> findById(String studentId);
 }
