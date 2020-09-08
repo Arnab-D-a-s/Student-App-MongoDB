519076cd2ddcc4360d8e2ae636aaf6f8c921a146
Arnab <arnabd2006@gmail.com>
2020-09-08 16:46:55 +0200

    Initial Commit

--- src/main/java/das/tech/studentapplication/dao/StudentData.java
+++ src/main/java/das/tech/studentapplication/dao/StudentData.java
@@ -3,14 +3,14 @@ package das.tech.studentapplication.dao;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Document
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class StudentData {
     @Id
     private String studentId;
--- src/main/java/das/tech/studentapplication/repository/StudentRepository.java
+++ src/main/java/das/tech/studentapplication/repository/StudentRepository.java
@@ -1,11 +1,9 @@
 package das.tech.studentapplication.repository;
 
 import das.tech.studentapplication.dao.StudentData;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Mono;
 
 @Repository
-public interface StudentRepository extends ReactiveCrudRepository<StudentData, String> {
-    Mono<StudentData> findById(String studentId);
+public interface StudentRepository extends MongoRepository<StudentData, String> {
 }
