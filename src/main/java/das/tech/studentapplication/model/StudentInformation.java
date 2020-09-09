0fcb51980cb5ad5d7adef803f6cb48765640b3a5
Arnab <arnabd2006@gmail.com>
2020-09-09 16:40:30 +0200

    Added the get endpoint

--- src/main/java/das/tech/studentapplication/model/StudentInformation.java
+++ src/main/java/das/tech/studentapplication/model/StudentInformation.java
@@ -1,9 +1,13 @@
 package das.tech.studentapplication.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder(toBuilder = true)
 public class StudentInformation {
     private String studentId;
--- src/main/java/das/tech/studentapplication/service/StudentController.java
+++ src/main/java/das/tech/studentapplication/service/StudentController.java
@@ -1,17 +1,19 @@
 package das.tech.studentapplication.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import das.tech.studentapplication.dao.StudentData;
+import das.tech.studentapplication.model.StudentInformation;
 import das.tech.studentapplication.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1")
@@ -25,9 +27,22 @@ public class StudentController {
     private final StudentRepository studentRepository;
 
     @PostMapping(path = "/students", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public String createStudents(@RequestBody StudentData studentInformation) {
-        studentRepository.save(studentInformation);
+    public String createStudents(@RequestBody StudentInformation studentInformation) throws JsonProcessingException {
+        studentInformation = studentInformation.toBuilder().studentId(UUID.randomUUID().toString()).build();
+        StudentData studentData = objectMapper.treeToValue(objectMapper.valueToTree(studentInformation),StudentData.class);
+        System.out.println(studentData.getStudentId());
+        studentRepository.save(studentData);
         return studentInformation.getStudentId();
+    }
+
+    @GetMapping(path = "/findStudents/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public StudentInformation createStudents( @PathVariable String id) throws JsonProcessingException {
+        Optional<StudentData> outPut = studentRepository.findById(id);
+        if (outPut.isPresent()){
+            StudentInformation studentData = objectMapper.treeToValue(objectMapper.valueToTree(outPut.get()),StudentInformation.class);
+            return studentData;
+        }
+        return StudentInformation.builder().studentName("None").build();
     }
 
 }
